b8cce7d5d11c869cf5984172b7598bacf2dcbb09
Susan Prestage <susan.prestage@gmail.com>
2014-04-09 14:49:43 -0700

    Add a model for votes.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140409214330) do
+ActiveRecord::Schema.define(version: 20140409214916) do
 
   create_table "topics", force: true do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "votes", force: true do |t|
+    t.integer  "topic_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
