198c0dc1e77ddc49d836edf5cd28df7d00f03ffe
Susan Prestage <susan.prestage@gmail.com>
2014-04-09 17:15:17 -0700

    Add a new controller action for down voting.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -73,6 +73,7 @@ class TopicsController < ApplicationController
     redirect_to(topics_path)
   end
 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_topic
