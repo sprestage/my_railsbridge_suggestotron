5a7298739cc8f83a31ffa9960ac7dd4a87fc8c69
Susan Prestage <susan.prestage@gmail.com>
2014-04-11 11:38:35 -0700

    Sort topics by most votes.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -5,6 +5,7 @@ class TopicsController < ApplicationController
   # GET /topics.json
   def index
     @topics = Topic.all
+    @topics.sort_by! {|topic| -topic.votes.count}
   end
 
   # GET /topics/1
